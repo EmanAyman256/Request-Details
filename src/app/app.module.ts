b4b66cfab15d6de2ee2de31ab3cad9f4e3c73d6d
EmanAyman256 <emanayman256@gmail.com>
2024-12-26 14:18:41 +0200

    add-navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,32 @@ import { TableModule } from 'primeng/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainRequestComponent } from './main-request/main-request.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ThemingService, ThemingModule } from '@fundamental-ngx/core/theming';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShellbarModule } from '@fundamental-ngx/core/shellbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainRequestComponent
+    MainRequestComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    TableModule
+    ShellbarModule,
+    TableModule,
+    NoopAnimationsModule,
+    ThemingModule.withConfig({ defaultTheme: 'sap_horizon', changeThemeOnQueryParamChange: false })
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+constructor(themingService: ThemingService) {
+themingService.init();
+}
+}
